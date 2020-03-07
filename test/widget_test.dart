fa6880f0aacac703c9f30e8207ba21057ddcb1cc
Scott <scottfarestrand@gmail.com>
2020-03-07 10:51:28 -0800

    Fixing Stuff c

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,9 +10,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:messio/main.dart';
 
 void main() {
-  testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-//    await tester.pumpWidget(Messio());
-//    expect(find.text('Hello World!'), findsOneWidget);
-  });
+//  testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
+//    // Build our app and trigger a frame.
+////    await tester.pumpWidget(Messio());
+////    expect(find.text('Hello World!'), findsOneWidget);
+//  });
 }
