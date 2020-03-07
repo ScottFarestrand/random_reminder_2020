444dee342e6d7f5cfc6e4344def9d4564caa3fd9
Scott <scottfarestrand@gmail.com>
2020-03-07 10:23:42 -0800

    Fixing Day 8 -A

--- lib/pages/ConversationPage.dart
+++ lib/pages/ConversationPage.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:messio/config/Palette.dart';
 import 'package:messio/widgets/ChatAppBar.dart';
 import 'package:messio/widgets/ChatListWidget.dart';
-import 'package:messio/widgets/InputWidget.dart';
 
 class ConversationPage extends StatefulWidget {
   @override
   _ConversationPageState createState() => _ConversationPageState();
+
+  const ConversationPage();
 }
 
 class _ConversationPageState extends State<ConversationPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-            appBar: ChatAppBar(), // Custom app bar for chat screen
-            body: Stack(children: <Widget>[
-              Column(
-                children: <Widget>[
-                  ChatListWidget(), //Chat list
-                  InputWidget() // The input widget
-                ],
-              ),
-            ])));
+    return Column(children: <Widget>[
+      Expanded(flex: 2, child: ChatAppBar()), // Custom app bar for chat screen
+      Expanded(
+          flex: 11,
+          child: Container(
+            color: Palette.chatBackgroundColor,
+            child: ChatListWidget(),
+          ))
+    ]);
   }
 }
